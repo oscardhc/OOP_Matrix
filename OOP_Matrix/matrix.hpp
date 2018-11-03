594a31556933fb1bf6f2b3d276523fa2032faa62
oscardhc <oscardhc@sjtu.edu.cn>
2018-11-03 16:39:47 +0800

    edit

--- OOP_Matrix/matrix.hpp
+++ OOP_Matrix/matrix.hpp
@@ -11,9 +11,34 @@ using std::size_t;
 namespace sjtu
 {
 	
-	template <class T>
-    class Matrix{
+    template <class T> class Chain{
+        T val;
+        Chain<T>* next;
+    private:
         
+        
+    public:
+        
+    };
+    
+    
+    template <class T> class Matrix{
+    private:
+        int N, M;
+        Chain<T*> rowHead;
+        
+        void init(){
+            for (int i = 1; i <= N; i++) {
+                
+            }
+        }
+    public:
+        Matrix(){
+            
+        }
+        Matrix(std::size_t n, std::size_t m, T init = T()){
+            
+        }
     };
 }
 

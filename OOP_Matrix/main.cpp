3fed8c819238a34920d4b80f2f5c857d00ac103b
oscardhc <oscardhc@sjtu.edu.cn>
2018-11-03 15:31:47 +0800

    Initial Commit

---
+++ OOP_Matrix/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OOP_Matrix
+//
+//  Created by Haichen Dong on 2018/11/3.
+//  Copyright © 2018 Haichen Dong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
